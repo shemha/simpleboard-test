5d65beec4f83b85dbef6208e86ea601ac7c785f4
shemha <yonagumori314@yahoo.co.jp>
2022-03-23 16:31:47 +0900

    deploy to heroku

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -3,8 +3,8 @@
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <script src="{{ mix('js/app.js') }}"></script>
     </head>
     <body>
         <!-- header.blade.phpを読み込む -->
